517e1fd13722023a214d8bcda81fe031445de5dc
Matan <matanamz@gmail.com>
2022-11-09 11:45:53 +0200

    addedddd

--- views/app-about.cmp.js
+++ views/app-about.cmp.js
@@ -2,6 +2,7 @@ export default {
 	template: `
         <section class="about-page">
             <h1>About us</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod dolorem </p>
         </section>
     `
 }
--- views/app-home.cmp.js
+++ views/app-home.cmp.js
@@ -1,7 +1,12 @@
 export default {
 	template: `
         <section class="home-page">
-            <h1>Home sweet home</h1>
+            <h1>Welcome to AppSus</h1>
+            <article className="apps-container debug">
+                <div className="book-app"></div>
+                <div className="email-app"></div>
+                <div className="note-app"></div>
+            </article>
         </section>
     `,
 }
