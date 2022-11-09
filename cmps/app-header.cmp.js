d7faa4d20df020e9cadc89c15aece69870ad0756
itai <itaimorag2000@gmail.com>
2022-11-09 11:37:06 +0200

    added routes

--- cmps/app-header.cmp.js
+++ cmps/app-header.cmp.js
@@ -4,7 +4,10 @@ export default {
             <h1>AppSus</h1>
             <nav>
                 <router-link to="/">Home</router-link> | 
-                <router-link to="/about">About</router-link>
+                <router-link to="/about">About</router-link>|
+                <router-link to="/keepApp">Keep</router-link>|
+                <router-link to="/emailApp">Email</router-link>|
+                <router-link to="/bookApp">Books Shop</router-link>
             </nav>
         </header>
     `,
--- views/apps/keep-app.cmp.js
+++ views/apps/keep-app.cmp.js
@@ -1,11 +1,7 @@
 export default {
     template: `
     <section class="car-app">
-        <!-- <car-filter @filter="setFilter"/> -->
-        <!-- <router-link to="/car/edit">Add a car</router-link> -->
-        <!-- <car-list 
-            @remove="removeCar" 
-            :cars="carsToShow"/> -->
+       
             <h1>hello</h1>
     </section>
     `,
