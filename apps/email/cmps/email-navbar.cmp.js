a134e2c73a25bee794b83477abec2ab9c811bd80
Matan <matanamz@gmail.com>
2022-11-10 13:54:49 +0200

    closing

--- apps/email/cmps/email-navbar.cmp.js
+++ apps/email/cmps/email-navbar.cmp.js
@@ -3,7 +3,7 @@ export default {
     <section @hover="addDelete" className="navbar-section">
         <button @click="renderNewEmail" class="new-email-btn"><i class="fa fa-pencil" style="font-size:18px"></i>New email</button>
         <button @click="changeCriteria('status', 'inbox')" class="income"><i class="fa fa-envelope-o"></i> Income mail</button>
-        <button class="income"><i class="fa fa-star-o"></i> Stared</button>
+        <button @click="changeCriteria('isStared', 'true')" class="income"><i class="fa fa-star-o"></i> Stared</button>
         <button @click="changeCriteria('status', 'sent')" class="income"> <i class="fa fa-send-o"></i> Sent</button>
         <button @click="changeCriteria('status', 'trash')" class="income"><i class="fa fa-trash-o"></i> Garbage </button>
         <button @click="changeCriteria('status', 'draft')" class="income"> <i class="fa fa-archive"> </i> Drafts</button>
--- apps/email/cmps/email-preview.cmp.js
+++ apps/email/cmps/email-preview.cmp.js
@@ -4,61 +4,73 @@ import { emailService } from '../services/email.service.js'
 export default {
     props: ['email'],
     template: `
-    <section @click="emailSelect(email)" :class="checkIfRead(email)" class="email-preview">
-        <button v-if="!email.isStared" @click.stop="starEmail(true)"><i class="fa fa-star-o"></i></button>
-        <button v-if="email.isStared" @click.stop="starEmail(false)"><i class="fa fa-star"></i></button>
-            <p class="bolded">From: {{substringSender}}</p>
+    <section @mouseleave="mouseOver(email, false)" @mouseover="mouseOver(email, true)" @click="emailSelect(email)" :class="checkIfRead(email)" class="email-preview">
+        <div v-if="!email.isStared" @click.stop="starEmail(true)"><i class="fa fa-star-o"></i></div>
+        <div v-if="email.isStared" @click.stop="starEmail(false)"><i class="fa fa-star"></i></div>
+            <p class="bolded">{{substringSender}}</p>
             <p class="bolded">To: {{substringReceiver}}</p>
         <p class="bold">{{ email.subject }}</p>
         <p class="body">{{ substringBody }}</p>
-        <p >{{formattedSeenAt}}</p>
+        <div v-if="email.isHovered && !email.isRead"  @click.stop="changeIsRead(email, false)"><i class="fa fa-envelope-o"></i></div>
+        <div v-if="email.isHovered && email.isRead"  @click.stop="changeIsRead(email, true)"><i class="fa fa-envelope-open-o"></i></div>
+        <div v-if="email.isHovered" @click.stop="draftEmail(true)"><i class="fa fa-archive"></i></div>
+        <p v-if="!email.isHovered">{{formattedSentAt}}</p>
     </section>
     <email-details @removed="" @replied="reply" :email="selectedEmail" v-if="isEmailOpen" />
     `,
     data() {
         return {
             selectedEmail: null,
-            isEmailOpen:false,
+            isEmailOpen: false,
         }
     },
     methods: {
+        changeIsRead(email, val){
+            email.isRead = val
+        },
+        mouseOver(email, val){
+            email.isHovered = val
+        },
+        draftEmail(email) {
+
+        },
         emailSelect(email) {
             email.isRead = true
             this.selectedEmail = email
             this.isEmailOpen = !this.isEmailOpen
             emailService.save(email)
         },
-        checkIfRead(email){
-            if(!email.isRead) return 'unread'
+        checkIfRead(email) {
+            if (!email.isRead) return 'unread'
             return 'read'
         },
-        reply(from){
+        reply(from) {
             console.log(from);
             this.$emit('replied', from)
         },
-        starEmail(val){
+        starEmail(val) {
             this.email.isStared = val
             emailService.save(this.email)
         }
     },
     computed: {
         substringSender() {
-            console.log(this.email.from, );
-            if(this.email.from === emailService.getLoggedInUser().email) return 'You'
+            console.log(this.email.from,);
+            if (this.email.from === emailService.getLoggedInUser().email) return 'You'
             let string = this.email.from
             let split = string.split("@");
             let value = split[0];
             return value
         },
         substringReceiver() {
-            console.log(this.email.to, );
-            if(this.email.to === emailService.getLoggedInUser().email) return 'You'
+            console.log(this.email.to,);
+            if (this.email.to === emailService.getLoggedInUser().email) return 'You'
             let string = this.email.to
             let split = string.split("@");
             let value = split[0];
             return value
         },
-        formattedSeenAt() {
+        formattedSentAt() {
             let currDay = new Date().getDay()
             let seen = this.email.sentAt
             let datedFormat = new Date(seen)
@@ -67,9 +79,9 @@ export default {
 
         },
 
-        substringBody(){
-            if(this.email.body.length >= 50)  return this.email.body.slice(0,50) + '...'
-           else return this.email.body
+        substringBody() {
+            if (this.email.body.length >= 50) return this.email.body.slice(0, 50) + '...'
+            else return this.email.body
         }
     },
 
