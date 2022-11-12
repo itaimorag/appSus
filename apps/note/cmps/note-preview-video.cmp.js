8f762d66d0cfc0791d758b4063edf70a7c9d3222
itai <itaimorag2000@gmail.com>
2022-11-12 19:24:00 +0200

    dasda

--- apps/note/cmps/note-preview-video.cmp.js
+++ apps/note/cmps/note-preview-video.cmp.js
@@ -2,6 +2,7 @@ export default {
     props: ['note'],
     template: `
         <section class="note-template">
+            <h2> {{ this.note.info.title }} </h2>
             <iframe width="100%" :src="videoSrcLink"></iframe>
         </section>
     `,
