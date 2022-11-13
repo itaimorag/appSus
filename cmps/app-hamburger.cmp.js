73ea545144d919b0b87e3af90598b222fdff2d60
Matan <matanamz@gmail.com>
2022-11-13 10:58:44 +0200

    asderhfbedfvwef

--- cmps/app-hamburger.cmp.js
+++ cmps/app-hamburger.cmp.js
@@ -5,9 +5,9 @@ export default {
         <section class="app-hamburger">
                 <router-link class="grid-item" to="/">Home</router-link>
                 <router-link class="grid-item" to="/about">About</router-link>
-                <router-link class="grid-item" to="/noteApp">Keep <img src="../imgs/keeps.png"/></router-link>
-                <router-link class="grid-item" to="/emailApp">Email <img src="../imgs/gmail.png"/></router-link>
-                <router-link class="grid-item" to="/bookApp">Books Shop <img src="../imgs/book-shop.png"/></router-link>
+                <router-link class="grid-item" to="/noteApp">Keep <img src="./imgs/keeps.png"/></router-link>
+                <router-link class="grid-item" to="/emailApp">Email <img src="./imgs/gmail.png"/></router-link>
+                <router-link class="grid-item" to="/bookApp">Books Shop <img src="./imgs/book-shop.png"/></router-link>
         
         </section>
     `,
