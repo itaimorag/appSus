a40f25d73fd9a8b39be2eb52f1f213f3e2701c95
Matan <matanamz@gmail.com>
2022-11-15 20:38:54 +0200

    fixed

--- apps/email/cmps/email-navbar.cmp.js
+++ apps/email/cmps/email-navbar.cmp.js
@@ -2,7 +2,7 @@ export default {
     template: `
     <section @hover="addDelete" className="navbar-section">
         <button :class="" @click="renderNewEmail" class="new-email-btn"><i class="fa fa-pencil" style="font-size:18px"></i>New email</button>
-        <button @click="changeCriteria('status', 'inbox')" class="income"><i class="fa fa-envelope-o"></i> Income mail</button>
+        <button @click="changeCriteria('status', 'inbox')" class="income"><i class="fa fa-envelope-o"></i> Inbox</button>
         <button @click="changeCriteria('isStared', 'true')" class="income"><i class="fa fa-star-o"></i> Stared</button>
         <button @click="changeCriteria('status', 'sent')" class="income"> <i class="fa fa-send-o"></i> Sent</button>
         <button @click="changeCriteria('status', 'trash')" class="income"><i class="fa fa-trash-o"></i> Garbage </button>
